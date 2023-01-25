9b85776284f8b1e1997fa9904528a7950cae412b
keshridivya <dkeshari094@gmail.com>
2023-01-25 16:48:12 +0530

    frontend template done

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'Welcome';
+$route['about']='welcome/about';
+$route['product']='welcome/product';
+$route['blog']='welcome/blog';
+$route['testimonial']='welcome/testimonial';
+$route['contact']='welcome/contact';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
